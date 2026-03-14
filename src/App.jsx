190729439d2bd528319806828ef7e8afa48f00d6
juhairulislam <juhairulislam2018@gmail.com>
2026-03-14 23:52:17 +0600

    project setup and react extension pack

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Countries from './components/countries/countries'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
      <h1>React On the go..</h1>
+     <Countries></Countries>
+
     </>
   )
 }
---
+++ src/components/countries/Countries.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>This is rsc</h1>
+            
+        </div>
+    );
+};
+
+export default Countries;
